24adced798994d96d1fef9a33a9ea1ff2e1b2f26
waiteman <mcclearr@gmail.com>
2025-02-11 10:51:57 -0500

    print report function

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def main(): 
     #open_frankenstein()
     current_book = "books/frankenstein.txt"
-    #print(count_words(current_book))
+    word_count = count_words(current_book)
     #print(book_to_strings(current_book))
     frankenstein = book_to_single_string(current_book)
-    print (count_characters(frankenstein))
+    char_dic = (count_characters(frankenstein))
+    char_dic_list = convert_dic_to_list(char_dic)
+    sorted_list = sort_list(char_dic_list)
+    print_report(current_book, sorted_list, word_count)
+    
     return None
 
 def open_frankenstein(): 
@@ -40,6 +44,25 @@ def count_characters(word_list):
                 char_dic[char] = 1
     return char_dic
             
+def convert_dic_to_list(char_dic):
+    char_list = []
+    for char in char_dic:
+        if char.isalpha():
+            char_list.append({"char": char, "num": char_dic[char]})
+    return char_list
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_list(char_dic_list):
+    char_dic_list.sort(reverse=True, key=sort_on)
+    return char_dic_list
+
+def print_report(book, list, count):
+    print(f"--- Begin report of {book} ---")
+    print(f"{count} words found in the document\n")
+    for item in list:
+        print(f"The '{item["char"]}' character was found {item["num"]} times")
 
 
 main()
