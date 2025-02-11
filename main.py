d3df4eac29ae45a87446361c42e79f7a6040677e
waiteman <mcclearr@gmail.com>
2025-02-11 10:16:03 -0500

    count characters function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def main(): 
     #open_frankenstein()
     current_book = "books/frankenstein.txt"
-    print(count_words(current_book))
+    #print(count_words(current_book))
+    #print(book_to_strings(current_book))
+    frankenstein = book_to_single_string(current_book)
+    print (count_characters(frankenstein))
     return None
 
 def open_frankenstein(): 
@@ -13,9 +16,30 @@ def count_words(book_file):
     with open(book_file) as f:
         file_contents = f.read()
         word_list = file_contents.split()
-        return len(word_list)
+    return len(word_list)
 
+def book_to_strings(book_file):
+    with open(book_file) as f:
+        file_contents = f.read()
+        word_list = file_contents.split()
+    return word_list
 
+def book_to_single_string(book_file):
+    with open(book_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_characters(word_list):
+    char_dic = {}
+    for word in word_list:
+        lowered_word = word.lower()
+        for char in lowered_word:
+            if char in char_dic:
+                char_dic[char] += 1
+            else:
+                char_dic[char] = 1
+    return char_dic
+            
 
 
 main()
